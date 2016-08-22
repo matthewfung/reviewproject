d0755760f4716e8991ca35a9783d7dd64d6a2668
Matthew Fung <matt.fung89@gmail.com>
2016-08-21 23:00:28 -0700

    Setup an Express Project with EJS template

--- app.js
+++ app.js
@@ -1,21 +1,62 @@
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-var http = require("http");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/myproject';
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	// Use connect method to connect to the server
-	MongoClient.connect(url, function(err, db) {
-	  assert.equal(null, err);
-	  console.log("Connected successfully to server");
+var app = express();
 
-	  db.close();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-	response.end('Minnie is a mouse');
-}).listen(8080);
+// error handlers
 
-console.log('Server running at http//localhost:8080')
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.listen(3000);
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
