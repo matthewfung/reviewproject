d94ccfc1799dd3c790a9cabce2059026816874de
Matthew Fung <matt.fung89@gmail.com>
2016-08-22 00:07:40 -0700

    Basic Angular integration

---
+++ public/javascripts/app.js
@@ -0,0 +1,19 @@
+var contactsApp = angular.module('contactsApp', []);
+
+contactsApp.controller('ContactsController', ['$scope', function($scope) {
+  $scope.contacts = [
+    {
+      name: 'Mickey Mouse',
+      description: 'The main character of Disney.',
+      phone: '425-123-4567'
+    }, {
+      name: 'Minnie Mouse',
+      description: 'Looks like Mickey Mouse except with eye lashes and a bow.',
+      phone: '425-234-5678'
+    }, {
+      name: 'Donald Duc',
+      description: 'Mickey and Minnie best friend.',
+      phone: '425-345-6789'
+    }
+  ];
+}]);
